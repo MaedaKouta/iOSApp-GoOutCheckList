932cda3ff40088ac065bcd8e268bd039100f431c
Kota <maedakoutaa@gmail.com>
2022-10-07 10:15:45 +0900

    FloatingPanelLayoutの実装

---
+++ iOSApp-GoOutCheckList/View/RegisterCategoryDetailView/RegisterCategoryDetailFloatingPanelLayout.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterCategoryDetailFloatingPanelLayout.swift
+//  iOSApp-GoOutCheckList
+//
+//  Created by 前田航汰 on 2022/10/07.
+//
+
+/*
+ カテゴリの登録の際に出現するセミモーダルのレイアウトファイル。
+ FloatingPanelのレイアウトで、4/3と1/2の２種類のセミモーダルを実装している。
+ */
+
+import Foundation
+import FloatingPanel
+
+class RegisterCategoryDetailFloatingPanelLayout: FloatingPanelLayout {
+    let position: FloatingPanelPosition = .bottom
+    let initialState: FloatingPanelState = .full
+    let anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] = [
+        .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
+        .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea)
+    ]
+}
