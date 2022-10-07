639b4b4e790cd0d86be2265fd07256f16a77acd0
Kota <maedakoutaa@gmail.com>
2022-10-07 10:16:07 +0900

    FloatingPanelでセミモーダルの実装完了

--- iOSApp-GoOutCheckList/View/CategoryListView/CategoryTableViewController.swift
+++ iOSApp-GoOutCheckList/View/CategoryListView/CategoryTableViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 import RxCocoa
 import RxSwift
 import RxRelay
+import FloatingPanel
 
-class CategoryTableViewController: UIViewController {
+class CategoryTableViewController: UIViewController, FloatingPanelControllerDelegate {
 
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var addCategoryButton: UIButton!
+    var fpc: FloatingPanelController!
 
     private let disposeBag = DisposeBag()
     private var categoryDataSource = CategoryDataSource()
@@ -26,6 +28,11 @@ class CategoryTableViewController: UIViewController {
         super.viewDidLoad()
         setupTableView()
         setupBindings()
+        setupFloatingPanel()
+
+        let view = RegisterCategoryDetailViewController()
+        fpc.set(contentViewController: view)
+        self.present(fpc, animated: true, completion: nil)
     }
 
     // MARK: TableView
@@ -43,6 +50,15 @@ class CategoryTableViewController: UIViewController {
 
     private func setupTableView() {
         tableView.register(UINib(nibName: "CategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CategoryTableViewCell")
+    }
+
+    private func setupFloatingPanel() {
+        fpc = FloatingPanelController(delegate: self)
+        fpc.layout = RegisterCategoryDetailFloatingPanelLayout()
+        fpc.isRemovalInteractionEnabled = true
+        let appearance = SurfaceAppearance()
+        appearance.cornerRadius = 24.0
+        fpc.surfaceView.appearance = appearance
     }
 
 }
