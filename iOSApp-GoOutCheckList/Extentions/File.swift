5fa82a4726a95750baa9240f6ee138991f85863d
Kota <maedakoutaa@gmail.com>
2022-10-07 11:15:28 +0900

    遷移元に値を渡すためにNotificationでの値渡しを追加

---
+++ iOSApp-GoOutCheckList/Extentions/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  iOSApp-GoOutCheckList
+//
+//  Created by 前田航汰 on 2022/10/07.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static var CategoryViewFromRegisterViewNotification = NSNotification.Name(rawValue: "CategoryViewFromRegisterViewNotification")
+}
