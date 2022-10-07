5fd9757ed62b3a46fd093d4f4a5cbb92696fb19f
Kota <maedakoutaa@gmail.com>
2022-10-07 13:59:47 +0900

    CategoryTableViewTestsのファイル作成

---
+++ iOSApp-GoOutCheckListTests/CategoryTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryTableViewTests.swift
+//  iOSApp-GoOutCheckListTests
+//
+//  Created by 前田航汰 on 2022/10/07.
+//
+
+import XCTest
+@testable import CategoryTableViewController
+
+final class CategoryTableViewTests: XCTestCase {
+
+    func test_tableViewが表示されること() {
+    }
+
+
+}
