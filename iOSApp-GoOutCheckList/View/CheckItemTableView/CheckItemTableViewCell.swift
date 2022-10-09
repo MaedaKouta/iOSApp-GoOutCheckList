691b7c8f0869a900ffc169c64fea77564e36d92b
Kota <maedakoutaa@gmail.com>
2022-10-10 08:44:29 +0900

    add: XIBでCheckItemTableViewCellのレイアウト作成

--- iOSApp-GoOutCheckList/View/CheckItemTableView/CheckItemTableViewCell.swift
+++ iOSApp-GoOutCheckList/View/CheckItemTableView/CheckItemTableViewCell.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class CheckItemTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var itemNameLabel: UILabel!
+    @IBOutlet private weak var checkImage: UIImageView!
+
+    func configure(name: String, isDone: Bool) {
+        itemNameLabel.text = name
+
+        if isDone {
+            checkImage.image = UIImage(systemName: "circle.fill")
+        } else {
+            checkImage.image = UIImage(systemName: "circle")
+        }
+    }
+
 }
