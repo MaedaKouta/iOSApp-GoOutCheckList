6da25a44c3cb3690d94857047f6b1636afc9db49
Kota <maedakoutaa@gmail.com>
2022-10-11 08:42:12 +0900

    add: CategoryTableViewTestsのテスト実装

--- iOSApp-GoOutCheckListTests/CategoryTableViewTests.swift
+++ iOSApp-GoOutCheckListTests/CategoryTableViewTests.swift
@@ -6,13 +6,31 @@
 //
 
 import XCTest
+import RealmSwift
 @testable import iOSApp_GoOutCheckList
 
 final class CategoryTableViewTests: XCTestCase {
 
-    let vc = CategoryTableViewController()
-//    func test_tableViewが表示されること() {
-//        XCTAssertNotNil(vc.categoryDataSource.item)
-//    }
+    private var vc: CategoryTableViewController!
+    private var categoryDataSource: CategoryDataSource!
+    private var tableView: UITableView!
+
+    override func setUp() {
+        vc = CategoryTableViewController()
+        vc.loadView()
+        categoryDataSource = vc.checkCategoryDataSource()
+        tableView = vc.tableView
+    }
+
+    func test_tableViewが表示されること() {
+        XCTAssertTrue(vc.view.subviews.contains(tableView))
+    }
+
+    func test_unknownImageが自動で入っていること() {
+        let category = Category()
+        category.name = "テスト"
+        categoryDataSource.item.append(category)
+        XCTAssertNotNil(categoryDataSource.item.last?.imageData)
+    }
 
 }
