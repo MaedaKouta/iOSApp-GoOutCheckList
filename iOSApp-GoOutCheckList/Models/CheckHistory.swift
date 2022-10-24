fb80aaeb138627e89de885eb42038ef896d12f20
Kota <maedakoutaa@gmail.com>
2022-10-24 14:43:05 +0900

    add: CheckHistoryのobjectを定義

--- iOSApp-GoOutCheckList/Models/CheckHistory.swift
+++ iOSApp-GoOutCheckList/Models/CheckHistory.swift
@@ -7,12 +7,17 @@
 /*
  チェックした履歴を保存しておく
  外出時に、あれチェックしたかな？って振り返るために作成
- 過去100件までを保存しておく
+ 過去30件までを保存しておく
  */
 
 import Foundation
 import RealmSwift
 
 public class CheckHistory: Object {
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var categoryName: String = ""
 
+    open var primaryKey: String {
+        return "date"
+    }
 }
