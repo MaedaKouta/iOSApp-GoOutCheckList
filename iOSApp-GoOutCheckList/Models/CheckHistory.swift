da708b0348c3692ad10e1480ccdc3443de96f126
Kota <maedakoutaa@gmail.com>
2022-10-24 14:22:12 +0900

    add: CheckHistoryのModelの追加

---
+++ iOSApp-GoOutCheckList/Models/CheckHistory.swift
@@ -0,0 +1,18 @@
+//
+//  CheckHistory.swift
+//  iOSApp-GoOutCheckList
+//
+//  Created by 前田航汰 on 2022/10/24.
+//
+/*
+ チェックした履歴を保存しておく
+ 外出時に、あれチェックしたかな？って振り返るために作成
+ 過去100件までを保存しておく
+ */
+
+import Foundation
+import RealmSwift
+
+public class CheckHistory: Object {
+
+}
