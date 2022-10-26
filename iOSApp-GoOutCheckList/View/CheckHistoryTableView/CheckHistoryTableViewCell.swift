c89351e15c604b3f9ca70b5d710d13bafca8930c
Kota <maedakoutaa@gmail.com>
2022-10-27 08:34:06 +0900

    add: CheckHistoryTableViewCellファイルの追加

---
+++ iOSApp-GoOutCheckList/View/CheckHistoryTableView/CheckHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckHistoryTableViewCell.swift
+//  iOSApp-GoOutCheckList
+//
+//  Created by 前田航汰 on 2022/10/27.
+//
+
+import UIKit
+
+class CheckHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
