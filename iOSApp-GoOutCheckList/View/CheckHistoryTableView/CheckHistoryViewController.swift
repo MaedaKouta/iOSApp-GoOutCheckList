5ccbc855d7b3c244684c0a66c246fbe911a49bf6
Kota <maedakoutaa@gmail.com>
2022-10-28 09:59:16 +0900

    add: checkHistoryのtableViewの表示完了

--- iOSApp-GoOutCheckList/View/CheckHistoryTableView/CheckHistoryViewController.swift
+++ iOSApp-GoOutCheckList/View/CheckHistoryTableView/CheckHistoryViewController.swift
@@ -17,17 +17,34 @@ class CheckHistoryViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     private let disposeBag = DisposeBag()
-    private var categoryDataSource = CategoryDataSource()
+    private var checkHistoryDataSource = CheckHistoryDataSource()
     private let realm = try! Realm()
-    
+
+    private lazy var checkHistoryViewModel = CheckHistoryViewModel(
+        tableViewItemSelectedObservable: tableView.rx.itemSelected.asObservable()
+    )
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         setupTableView()
+        setupBindings()
     }
 
     private func setupTableView() {
         tableView.register(UINib(nibName: "CheckHistoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CheckHistoryTableViewCell")
+    }
+
+    private func setupBindings() {
+        // TableViewのデータ連携
+        checkHistoryViewModel.outputs.checkHistoryDataBehaviorRelay
+            .bind(to: tableView.rx.items(dataSource: checkHistoryDataSource))
+            .disposed(by: disposeBag)
+
+        // TODO: TableViewの再レンダリングにより、色が少しずつ薄くなる挙動にならない
+        checkHistoryViewModel.outputs.tableViewItemSeletedPublishRelay
+            .subscribe { [weak self] indexPath in
+                self?.tableView.deselectRow(at: indexPath, animated: true)
+            }.disposed(by: disposeBag)
     }
 
 }
