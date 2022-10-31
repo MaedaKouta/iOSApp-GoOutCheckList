d8021ec539513956866db4904c181fa97f3ba194
Kota <maedakoutaa@gmail.com>
2022-10-31 11:51:24 +0900

    fix: ViewControllerSafeareのバグ解決のためSettingVC再作成

--- iOSApp-GoOutCheckList/View/SettingTableView/SettingViewController.swift
+++ iOSApp-GoOutCheckList/View/SettingTableView/SettingViewController.swift
@@ -5,9 +5,11 @@
 //  Created by 前田航汰 on 2022/10/31.
 //
 
+
 import UIKit
 
 class SettingViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +19,9 @@ class SettingViewController: UIViewController {
 
     // MARK: Actions
 
-
     // MARK: Setups
     private func setupNavigationbar() {
         navigationItem.title = "設定"
     }
-
 
 }
