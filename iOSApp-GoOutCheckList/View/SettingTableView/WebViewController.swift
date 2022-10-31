c210ec38ba45ac1accd524711e644b74f8a97b4b
Kota <maedakoutaa@gmail.com>
2022-11-01 08:31:18 +0900

    add: webVCの完成

--- iOSApp-GoOutCheckList/View/SettingTableView/WebViewController.swift
+++ iOSApp-GoOutCheckList/View/SettingTableView/WebViewController.swift
@@ -12,15 +12,10 @@ import PKHUD
 class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     @IBOutlet weak var webView: WKWebView!
-    //@IBOutlet private weak var backButton: UIBarButtonItem!
-    //@IBOutlet private  weak var forwardButton: UIBarButtonItem!
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    @IBOutlet private  weak var forwardButton: UIBarButtonItem!
     private var presentUrl = ""
     private var progressView = UIProgressView(progressViewStyle: .bar)
-
-    func configure(presentUrl: String, navigationTitle: String) {
-        self.presentUrl = presentUrl
-        self.navigationItem.title = title
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +25,7 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
         HUD.show(.progress, onView: view)
         if let url = URL(string: presentUrl) {
-            self.webView?.load(URLRequest(url: url))
+            self.webView.load(URLRequest(url: url))
         } else {
             print("URLが取得できませんでした。")
         }
@@ -39,7 +34,6 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     @IBAction func didTapBackButton(_ sender: Any) {
         webView.goBack()
-
     }
 
     @IBAction func didTapForwardButton(_ sender: Any) {
@@ -58,33 +52,22 @@ class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     }
 
     private func judgeToolBarButton() {
-//        if webView.canGoBack {
-//            backButton.isEnabled = true
-//        } else {
-//            backButton.isEnabled = false
-//        }
-//
-//        if webView.canGoForward {
-//            forwardButton.isEnabled = true
-//        } else {
-//            forwardButton.isEnabled = false
-//        }
-    }
-
-    private func setupProgressView() {
-        guard let navigationBarH = self.navigationController?.navigationBar.frame.size.height else {
-            assertionFailure()
-            return
+        if webView.canGoBack {
+            backButton.isEnabled = true
+        } else {
+            backButton.isEnabled = false
         }
-        progressView = UIProgressView(frame: CGRect(x: 0.0, y: navigationBarH, width: self.view.frame.size.width, height: 0.0))
-        navigationController?.navigationBar.addSubview(progressView)
-        //変更を検知
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+
+        if webView.canGoForward {
+            forwardButton.isEnabled = true
+        } else {
+            forwardButton.isEnabled = false
+        }
     }
 
-//    @IBAction func didTapExitButton(_ sender: Any) {
-//        self.dismiss(animated: true, completion: nil)
-//    }
+    func configure(presentUrl: String, navigationTitle: String) {
+        self.presentUrl = presentUrl
+        self.navigationItem.title = navigationTitle
+    }
 
 }
