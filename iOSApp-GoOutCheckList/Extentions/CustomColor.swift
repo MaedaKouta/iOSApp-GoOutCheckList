8f4f644344193811a0d7bb24267f117245290051
Kota <maedakoutaa@gmail.com>
2022-11-19 10:39:26 +0900

    add: ダークモードに関わるUIColorのExtention追加

---
+++ iOSApp-GoOutCheckList/Extentions/CustomColor.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColor.swift
+//  iOSApp-GoOutCheckList
+//
+//  Created by 前田航汰 on 2022/11/19.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+    static var customMainColor = UIColor(named: "mainColor")
+    static var customSubColor = UIColor(named: "subColor")
+    static var customButtonBackGroundColor = UIColor(named: "buttonBackgroundColor")
+
+}
