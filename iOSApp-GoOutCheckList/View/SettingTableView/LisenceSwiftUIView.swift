59cf014c8e46504d81d2ad471d0eb3f03bde5426
Kota <maedakoutaa@gmail.com>
2022-11-20 23:18:00 +0900

    fix: ライセンスViewのNavigtaionバグ修正

--- iOSApp-GoOutCheckList/View/SettingTableView/LisenceSwiftUIView.swift
+++ iOSApp-GoOutCheckList/View/SettingTableView/LisenceSwiftUIView.swift
@@ -10,50 +10,48 @@ import SwiftUI
 struct LisenceSwiftUIView: View {
     
     var body: some View {
-
-        NavigationView {
-
-            VStack {
-
-                ScrollView {
-
-                    LisenceLabel(
-                        title: "ライセンス",
-                        bodyText: """
+        
+        VStack {
+            
+            ScrollView {
+                
+                LisenceLabel(
+                    title: "ライセンス",
+                    bodyText: """
                     本アプリは以下のオープンソースライブラリを使用しています
                     """)
-                    Divider()
-
-                    Group {
-                        LisenceLabel(
-                            title: "RxSwift",
-                            bodyText: """
+                Divider()
+                
+                Group {
+                    LisenceLabel(
+                        title: "RxSwift",
+                        bodyText: """
                     The MIT License Copyright © 2015 Krunoslav Zaher, Shai Mishali All rights reserved.
-
+                    
                     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
+                    
                     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+                    
                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                     """)
-                        Divider()
-                        LisenceLabel(
-                            title: "PKHUD",
-                            bodyText: """
+                    Divider()
+                    LisenceLabel(
+                        title: "PKHUD",
+                        bodyText: """
                     The MIT License (MIT)
-
+                    
                     Copyright (c) 2014 Philip Kluz (Philip.Kluz@gmail.com)
-
+                    
                     Permission is hereby granted, free of charge, to any person obtaining a copy
                     of this software and associated documentation files (the "Software"), to deal
                     in the Software without restriction, including without limitation the rights
                     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
                     copies of the Software, and to permit persons to whom the Software is
                     furnished to do so, subject to the following conditions:
-
+                    
                     The above copyright notice and this permission notice shall be included in all
                     copies or substantial portions of the Software.
-
+                    
                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -62,24 +60,24 @@ struct LisenceSwiftUIView: View {
                     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
                     SOFTWARE.
                     """)
-                        Divider()
-                        LisenceLabel(
-                            title: "FloatingPanel",
-                            bodyText: """
+                    Divider()
+                    LisenceLabel(
+                        title: "FloatingPanel",
+                        bodyText: """
                     MIT License
-
+                    
                     Copyright (c) 2018-Present Shin Yamamoto
-
+                    
                     Permission is hereby granted, free of charge, to any person obtaining a copy
                     of this software and associated documentation files (the "Software"), to deal
                     in the Software without restriction, including without limitation the rights
                     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
                     copies of the Software, and to permit persons to whom the Software is
                     furnished to do so, subject to the following conditions:
-
+                    
                     The above copyright notice and this permission notice shall be included in all
                     copies or substantial portions of the Software.
-
+                    
                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -88,24 +86,24 @@ struct LisenceSwiftUIView: View {
                     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
                     SOFTWARE.
                     """)
-                        Divider()
-                        LisenceLabel(
-                            title: "realm-swift",
-                            bodyText: """
+                    Divider()
+                    LisenceLabel(
+                        title: "realm-swift",
+                        bodyText: """
                                                      Apache License
                                                Version 2.0, January 2004
                                             http://www.apache.org/licenses/
-
+                    
                        TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
-
+                    
                        1. Definitions.
-
+                    
                           "License" shall mean the terms and conditions for use, reproduction,
                           and distribution as defined by Sections 1 through 9 of this document.
-
+                    
                           "Licensor" shall mean the copyright owner or entity authorized by
                           the copyright owner that is granting the License.
-
+                    
                           "Legal Entity" shall mean the union of the acting entity and all
                           other entities that control, are controlled by, or are under common
                           control with that entity. For the purposes of this definition,
@@ -113,24 +111,24 @@ struct LisenceSwiftUIView: View {
                           direction or management of such entity, whether by contract or
                           otherwise, or (ii) ownership of fifty percent (50%) or more of the
                           outstanding shares, or (iii) beneficial ownership of such entity.
-
+                    
                           "You" (or "Your") shall mean an individual or Legal Entity
                           exercising permissions granted by this License.
-
+                    
                           "Source" form shall mean the preferred form for making modifications,
                           including but not limited to software source code, documentation
                           source, and configuration files.
-
+                    
                           "Object" form shall mean any form resulting from mechanical
                           transformation or translation of a Source form, including but
                           not limited to compiled object code, generated documentation,
                           and conversions to other media types.
-
+                    
                           "Work" shall mean the work of authorship, whether in Source or
                           Object form, made available under the License, as indicated by a
                           copyright notice that is included in or attached to the work
                           (an example is provided in the Appendix below).
-
+                    
                           "Derivative Works" shall mean any work, whether in Source or Object
                           form, that is based on (or derived from) the Work and for which the
                           editorial revisions, annotations, elaborations, or other modifications
@@ -138,7 +136,7 @@ struct LisenceSwiftUIView: View {
                           of this License, Derivative Works shall not include works that remain
                           separable from, or merely link (or bind by name) to the interfaces of,
                           the Work and Derivative Works thereof.
-
+                    
                           "Contribution" shall mean any work of authorship, including
                           the original version of the Work and any modifications or additions
                           to that Work or Derivative Works thereof, that is intentionally
@@ -152,18 +150,18 @@ struct LisenceSwiftUIView: View {
                           Licensor for the purpose of discussing and improving the Work, but
                           excluding communication that is conspicuously marked or otherwise
                           designated in writing by the copyright owner as "Not a Contribution."
-
+                    
                           "Contributor" shall mean Licensor and any individual or Legal Entity
                           on behalf of whom a Contribution has been received by Licensor and
                           subsequently incorporated within the Work.
-
+                    
                        2. Grant of Copyright License. Subject to the terms and conditions of
                           this License, each Contributor hereby grants to You a perpetual,
                           worldwide, non-exclusive, no-charge, royalty-free, irrevocable
                           copyright license to reproduce, prepare Derivative Works of,
                           publicly display, publicly perform, sublicense, and distribute the
                           Work and such Derivative Works in Source or Object form.
-
+                    
                        3. Grant of Patent License. Subject to the terms and conditions of
                           this License, each Contributor hereby grants to You a perpetual,
                           worldwide, non-exclusive, no-charge, royalty-free, irrevocable
@@ -179,24 +177,24 @@ struct LisenceSwiftUIView: View {
                           or contributory patent infringement, then any patent licenses
                           granted to You under this License for that Work shall terminate
                           as of the date such litigation is filed.
-
+                    
                        4. Redistribution. You may reproduce and distribute copies of the
                           Work or Derivative Works thereof in any medium, with or without
                           modifications, and in Source or Object form, provided that You
                           meet the following conditions:
-
+                    
                           (a) You must give any other recipients of the Work or
                               Derivative Works a copy of this License; and
-
+                    
                           (b) You must cause any modified files to carry prominent notices
                               stating that You changed the files; and
-
+                    
                           (c) You must retain, in the Source form of any Derivative Works
                               that You distribute, all copyright, patent, trademark, and
                               attribution notices from the Source form of the Work,
                               excluding those notices that do not pertain to any part of
                               the Derivative Works; and
-
+                    
                           (d) If the Work includes a "NOTICE" text file as part of its
                               distribution, then any Derivative Works that You distribute must
                               include a readable copy of the attribution notices contained
@@ -213,14 +211,14 @@ struct LisenceSwiftUIView: View {
                               or as an addendum to the NOTICE text from the Work, provided
                               that such additional attribution notices cannot be construed
                               as modifying the License.
-
+                    
                           You may add Your own copyright statement to Your modifications and
                           may provide additional or different license terms and conditions
                           for use, reproduction, or distribution of Your modifications, or
                           for any such Derivative Works as a whole, provided Your use,
                           reproduction, and distribution of the Work otherwise complies with
                           the conditions stated in this License.
-
+                    
                        5. Submission of Contributions. Unless You explicitly state otherwise,
                           any Contribution intentionally submitted for inclusion in the Work
                           by You to the Licensor shall be under the terms and conditions of
@@ -228,12 +226,12 @@ struct LisenceSwiftUIView: View {
                           Notwithstanding the above, nothing herein shall supersede or modify
                           the terms of any separate license agreement you may have executed
                           with Licensor regarding such Contributions.
-
+                    
                        6. Trademarks. This License does not grant permission to use the trade
                           names, trademarks, service marks, or product names of the Licensor,
                           except as required for reasonable and customary use in describing the
                           origin of the Work and reproducing the content of the NOTICE file.
-
+                    
                        7. Disclaimer of Warranty. Unless required by applicable law or
                           agreed to in writing, Licensor provides the Work (and each
                           Contributor provides its Contributions) on an "AS IS" BASIS,
@@ -243,7 +241,7 @@ struct LisenceSwiftUIView: View {
                           PARTICULAR PURPOSE. You are solely responsible for determining the
                           appropriateness of using or redistributing the Work and assume any
                           risks associated with Your exercise of permissions under this License.
-
+                    
                        8. Limitation of Liability. In no event and under no legal theory,
                           whether in tort (including negligence), contract, or otherwise,
                           unless required by applicable law (such as deliberate and grossly
@@ -255,7 +253,7 @@ struct LisenceSwiftUIView: View {
                           work stoppage, computer failure or malfunction, or any and all
                           other commercial damages or losses), even if such Contributor
                           has been advised of the possibility of such damages.
-
+                    
                        9. Accepting Warranty or Additional Liability. While redistributing
                           the Work or Derivative Works thereof, You may choose to offer,
                           and charge a fee for, acceptance of support, warranty, indemnity,
@@ -266,27 +264,27 @@ struct LisenceSwiftUIView: View {
                           defend, and hold each Contributor harmless for any liability
                           incurred by, or claims asserted against, such Contributor by reason
                           of your accepting any such warranty or additional liability.
-
+                    
                        END OF TERMS AND CONDITIONS
                     """)
-                        Divider()
-                        LisenceLabel(
-                            title: "lottie",
-                            bodyText: """
+                    Divider()
+                    LisenceLabel(
+                        title: "lottie",
+                        bodyText: """
                     MIT License
-
+                    
                     Copyright (c) 2017 Airbnb
-
+                    
                     Permission is hereby granted, free of charge, to any person obtaining a copy
                     of this software and associated documentation files (the "Software"), to deal
                     in the Software without restriction, including without limitation the rights
                     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
                     copies of the Software, and to permit persons to whom the Software is
                     furnished to do so, subject to the following conditions:
-
+                    
                     The above copyright notice and this permission notice shall be included in all
                     copies or substantial portions of the Software.
-
+                    
                     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -295,7 +293,6 @@ struct LisenceSwiftUIView: View {
                     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
                     SOFTWARE.
                     """)
-                    }
                 }
             }
         }
@@ -305,14 +302,14 @@ struct LisenceSwiftUIView: View {
 private struct LisenceLabel: View {
     var title: String
     var bodyText: String
-
+    
     var body: some View {
         VStack {
             Text(title)
                 .bold()
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.bottom, 5)
-
+            
             Text(bodyText)
                 .font(.caption)
                 .foregroundColor(.init(red: 0.4, green: 0.4, blue: 0.4))
